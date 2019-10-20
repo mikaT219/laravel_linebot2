baece7cf8db870381f1288ed3173d0fe43d1c485
Your Name <you@example.com>
2019-10-20 17:38:28 +0900

    add debugbar controllers

--- app/Http/Controllers/Api/LineWebhookController.php
+++ app/Http/Controllers/Api/LineWebhookController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 //Log::info(‘ログ出力テスト’);
-//use Log;//一番上でね
+use Log;//一番上でね
 //Log::debug($data);
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
